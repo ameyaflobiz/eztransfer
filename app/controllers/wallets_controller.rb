3380d5753b585ceffc9a6fbad635f5b047d34e5a
ameyaflobiz <ameya@flobiz.in>
2022-01-18 09:18:53 +0530

    Added strong params to wallet and implemented optimistic locking

--- app/controllers/wallets_controller.rb
+++ app/controllers/wallets_controller.rb
@@ -3,8 +3,8 @@ class WalletsController < ApplicationController
 
     def add_money_to_wallet
     
-        
-        wallet = Wallet.fetch_wallet(@user.id, params[:currency_type])
+        params[:user_id] = @user.id
+        wallet = Wallet.fetch_wallet(wallet_params)
         amount = params[:amount]
        
         @wallet = WalletService.new().add_money(wallet.id,amount)
@@ -13,21 +13,23 @@ class WalletsController < ApplicationController
     end
 
     def transfer_money
-        # Call your service here no need
 
-
-        @transaction = WalletService.new().transfer_money(transfer_params)
+        transaction = WalletService.new().transfer_money(transfer_params)
 
         sender_wallet = Wallet.find_by(user_id: params[:sender_id], currency_type: params[:sender_currency_type] )
         reciever_wallet = Wallet.find_by(user_id: params[:reciever_id], currency_type: params[:reciever_currency_type] )
         
-        render json: { transaction: @transaction }
+        render json: { transaction: transaction }
     end
 
     private
 
     def transfer_params
         params.permit(:sender_id, :reciever_id, :sender_currency_type, :reciever_currency_type, :sender_amount)
+    end
+
+    def wallet_params
+        params.permit(:user_id,:currency_type)
     end
     
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_01_12_090831) do
+ActiveRecord::Schema.define(version: 2022_01_18_034625) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -44,6 +44,7 @@ ActiveRecord::Schema.define(version: 2022_01_12_090831) do
     t.bigint "user_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "lock_version"
     t.index ["user_id"], name: "index_wallets_on_user_id"
   end
 
